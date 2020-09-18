202bb84ef2944c8b42af403758413d35f570c8fe
Nayra <nayradb@gmail.com>
2020-09-18 12:31:09 +0200

    Agregar archivos de proyecto.

---
+++ PruebaASPNET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PruebaASPNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
